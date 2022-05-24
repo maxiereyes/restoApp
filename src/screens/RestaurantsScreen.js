72e8dc1b6af146e0a6e0ea5c7188f655f160cce9
Maxrey88 <maximiliano.reyes@aicore.com.ar>
2022-05-24 16:00:43 -0300

    Add first five screens primaries

---
+++ src/screens/RestaurantsScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const RestaurantsScreen = () => {
+  return (
+    <View>
+      <Text>Restaurants</Text>
+    </View>
+  )
+}
+
+export default RestaurantsScreen
+
+const styles = StyleSheet.create({})
